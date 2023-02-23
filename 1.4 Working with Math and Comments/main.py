d3240ed118538cadf8b65db4066cd881196bb8e0
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-02-23 09:44:44 -0500

    Add files via upload

---
+++ 1.4 Working with Math and Comments/main.py
@@ -0,0 +1,28 @@
+#Math calculator using 3 functions from the math library. Created By Lucas Leung for ICS3U course, TLK S.S., Feb 22 2023
+
+import math
+
+######SQUARE ROOT FUNCTION######
+#getting input from user, convert to int
+sqrtRequested = int(input('Enter a number to find the squareroot of it: '))
+
+#calculating the squareroot of the chosen number
+sqrtAns = math.sqrt(sqrtRequested)
+
+#resulting print statement
+print(f'The squareroot of {sqrtRequested} is {sqrtAns}')
+
+######SINE FUNCTION######
+#getting integer input from user to know what angle is to be used
+sineRequested = int(input('Enter an angle to find the sine of it, in both degrees and radians: '))
+
+#calculates sine of chosen angle in radians.
+sineAnsInRadians = math.sin(sineRequested)
+
+#convert angle from degrees to radians and calculate sine
+sineRequestedInRadians = math.radians(sineRequested)
+sineAnsInDegrees = math.sin(sineRequestedInRadians)
+
+#resulting print statement
+print(f'Degrees: The sin of {sineRequested} is {sineAnsInDegrees}')
+print(f'Radians: The sin of {sineRequested} is {sineAnsInRadians}')
