0c16278ca05ba9a4fe5be1fb7a11419e9249c324
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-03-01 17:05:36 -0500

    Add files via upload

---
+++ 2.3 - Boolean Operators e.g. AND, OR, NOT/instructions.md
@@ -0,0 +1,7 @@
+# Instructions  
+
+1. Write a program that asks for 3 numbers. The program should then print a statement about if the 3rd number is between the first 2. For example 2, 6 and 10 would print "10 is not between 2 and 6", but 2 100 and 80 would print 80 is between 2 and 100
+2. Write a program that greets people. Your program should have 4 different greetings and 2 of them should be specific to multiple people. For example if the input is "Marco" there is one response, if the input is "Miller", "Brayden", or "Gabriel" there is a second input and if your input is "Omar", "Tavish", or "Rhedj" there is an other output.
+3. Write a program that takes in a string, and returns true if that string contains all the letter of your first name.
+
+  
---
+++ 2.3 - Boolean Operators e.g. AND, OR, NOT/main.py
@@ -0,0 +1,30 @@
+
+#part 1 - asks for 3 numbers and checks if third number is between first and second number
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+num3 = int(input('Enter third number: '))
+if (num3 < num2 and num3 > num1):
+  print(f'{num3} is between {num1} and {num2}')
+  print('')
+else:
+  print(f'{num3} is not between {num1} and {num2}')
+  print('')
+
+#part 2 - greets people, different greetings depending on the name inputted
+name = input('What is your name?: ').capitalize()
+if name == 'Biden':
+  print('Welcome, Mr. President')
+elif name == 'Joe' or name == 'Mari' or name == 'Kel' or name == 'Sunny':
+  print(f'how are ya, {name}')
+elif name == 'Miller' or name == 'Adam' or name == 'Gurpreet':
+  print(f'Hello there, {name}.')
+elif name == 'Niko':
+  print(f'Hey {name} o/')
+
+#part 3 - asks for a name and check if it contains all the letters of my name
+secondName = input('Choose another first name: ').lower()
+
+if 'l' in secondName and 'u' in secondName and 'c' in secondName and 'a' in secondName and 's' in secondName:
+  print('Are you trying to commit identity theft?')
+else:
+  print('You are not Lucas')
