56c5e0fdb42057eae5e0627e280fe53b88952514
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-05-13 15:45:47 -0400

    Delete 1 - Hello World directory

--- 1 - Hello World/main.py
+++
@@ -1,5 +0,0 @@
-#Added variable by referring to the docs. Change the name to whatever your name is :) #
-name = 'Lucas'
-def hello_my_name_is():
-  print("hello, my name is", name)
-hello_my_name_is()
